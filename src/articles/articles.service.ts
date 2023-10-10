819d25b954dc437dcf5e2c779e5681682e27e0f3
Higor Moriyama <higor.moriyama@atos.net>
2023-10-09 21:26:03 -0300

    CRUD operations: DONE

---
+++ src/articles/articles.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { UpdateArticleDto } from './dto/update-article.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class ArticlesService {
+
+  constructor(private prisma: PrismaService){}
+
+  async create(createArticleDto: CreateArticleDto) {
+    return await this.prisma.article.create({ data: createArticleDto });
+  }
+
+  findAll() {
+    return this.prisma.article.findMany({ where: { published: true } });
+  }
+
+  findDrafts(){
+    return this.prisma.article.findMany({ where: { published: false } });
+  }
+
+  async findOne(id: number) {
+    // return `This action returns a #${id} article`;
+    return this.prisma.article.findUnique({ where: { id }});
+  }
+
+  update(id: number, updateArticleDto: UpdateArticleDto) {
+    return this.prisma.article.update({ where: {id}, data: updateArticleDto });
+  }
+
+  remove(id: number) {
+    return this.prisma.article.delete({ where: { id }});
+  }
+}
